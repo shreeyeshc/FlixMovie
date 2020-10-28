a3505d8af626f4a318b88606155b80ca669503c4
shreeyesh <shreeyeshc@gmail.com>
2020-10-27 21:48:55 -0500

    Committed

---
+++ FlixMovie/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixMovie
+//
+//  Created by Shreeyesh Chauhan on 10/27/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
